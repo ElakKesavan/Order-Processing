c87ea350743d653fc48cd023b83ff86e110f580a
Kesavan <elakesavan@gmail.com>
2026-03-26 20:52:48 +0530

    Wrapping Auth queries in Transaction

--- src/main/java/com/peerislands/orders/service/AuthServiceImpl.java
+++ src/main/java/com/peerislands/orders/service/AuthServiceImpl.java
@@ -17,6 +17,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -30,6 +31,7 @@ public class AuthServiceImpl implements AuthService {
 
     private final JwtUtils jwtUtils;
 
+    @Transactional
     public void register(@NotNull @Valid SignupRequest signUpRequest) {
         if (userRepository.existsByEmail(signUpRequest.getEmail())) {
             throw new IllegalArgumentException("Error: Email is already in use!");
@@ -45,6 +47,7 @@ public class AuthServiceImpl implements AuthService {
         userRepository.save(user);
     }
 
+    @Transactional
     public JwtResponse authenticate(@NotNull @Valid LoginRequest loginRequest) {
         Authentication authentication =
                 authenticationManager.authenticate(
